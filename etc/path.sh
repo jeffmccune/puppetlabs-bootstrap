514c3681aca3a1da2a1c2a857fc20fe842bffe33
Jeff McCune <jeff@puppetlabs.com>
2010-07-09 15:38:53 -0700

    Added path.sh to statically assign a default PATH
    This PATH is assumed to work for root on Enterprise Linux
    based distributions.

---
+++ etc/path.sh
@@ -0,0 +1,9 @@
+# 2010-07-09 Jeff McCune <jeff@puppetlabs.com>
+#
+# Setup a sensible base path.
+#
+
+PATH="/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
+PATH="${FACTER_PLABS_PREFIX}/bin:${FACTER_PLABS_PREFIX}/puppetlabs-bootstrap/bin:${PATH}"
+export PATH
+
