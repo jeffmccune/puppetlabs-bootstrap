18240c2694165c51cc689a2658f7fd730ff7bb18
Jeff McCune <jeff@puppetlabs.com>
2010-07-09 15:29:31 -0700

    Added runsuite logic to execute a command.
    All other wrapper scripts should utilize this script.

--- etc/functions.sh
+++ etc/functions.sh
@@ -12,7 +12,21 @@ set -u
 : ${FACTER_PLABS_PREFIX:=/opt/puppetlabs}
 export FACTER_PLABS_PREFIX
 
+# Set the default suite as a facter fact if not already set
+: ${FACTER_PLABS_SUITE:=bootstrap}
+export FACTER_PLABS_SUITE
+
 ## Functions ##
+SetupEnviornment() {
+  # This function sets up RUBYLIB and PATH
+  if [ -z "${facter_plabs_suite_envissetup:-}" ]; then
+    facter_plabs_suite_envissetup=true
+    PATH="${FACTER_PLABS_PREFIX}/suite/${FACTER_PLABS_SUITE}/bin:${PATH}"
+    RUBYLIB="${FACTER_PLABS_PREFIX}/suite/${FACTER_PLABS_SUITE}/lib:${RUBYLIB}"
+    export PATH RUBYLIB
+  fi
+}
+
 Debug() {
   # Write a message to STDERR if DEBUG is "true"
   if [ ${DEBUG:-false} == "true" ]; then
