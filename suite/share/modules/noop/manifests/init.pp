7e9454da64cfd74a9742ceba016a80d6a414e689
Jeff McCune <jeff@puppetlabs.com>
2010-07-09 15:18:32 -0700

    Added noop module in the shared suite.
    The shared suite will be for modules common across all suites.
    Ideally, every module would be here.  These modules should
    come from the Forge without modification.

---
+++ suite/share/modules/noop/manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: noop
+#
+#   2010-07-09 Jeff McCune <jeff@puppetlabs.com>
+#   Example module class that does nothing.
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class noop {
+  $module = "noop"
+}
