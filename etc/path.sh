18d291e06dfa71665f2320721e88a2f49cfe138d
Jeff McCune <jeff@puppetlabs.com>
2010-07-09 15:43:44 -0700

    Fixup missing FACTER_PLABS_PREFIX default

--- etc/path.sh
+++ etc/path.sh
@@ -3,7 +3,9 @@
 # Setup a sensible base path.
 #
 
+# Set default prefix
+: ${FACTER_PLABS_PREFIX:=/opt/puppetlabs}
+
 PATH="/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
 PATH="${FACTER_PLABS_PREFIX}/bin:${FACTER_PLABS_PREFIX}/puppetlabs-bootstrap/bin:${PATH}"
 export PATH
-
