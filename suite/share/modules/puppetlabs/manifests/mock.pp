a1f58eb71ab8203aa602c8d150b142950019fec3
Jeff McCune <jeff@puppetlabs.com>
2010-07-26 20:42:39 -0600

    Added mock class.
    This class will setup a system to run Fedora Mock
    package builds.
    
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

---
+++ suite/share/modules/puppetlabs/manifests/mock.pp
@@ -0,0 +1,40 @@
+# Class: puppetlabs::mock
+#
+#   This class sets up the mock build system on a host.
+#   http://fedoraproject.org/wiki/Projects/Mock
+#
+#   Mock creates chroots and builds packages in them.
+#   Its only task is to reliably populate a chroot and
+#   attempt to build a package in that chroot.
+#
+# Parameters:
+#
+# Actions:
+#
+#  Manage User[mock]
+#  Manage Group[mock]
+#  Manage Package[mock]
+#
+# Requires:
+#
+# Sample Usage:
+#
+class puppetlabs::mock {
+  $module = "puppetlabs"
+  # statements
+  user {
+    "mock":
+      ensure     => "present",
+      home       => "/var/lib/mock",
+      comment    => "Fedora Mock Build",
+      password   => '!!',
+      shell      => "/bin/bash",
+  }
+  group { "mock": ensure => "present" }
+  package {
+    "mock":
+      ensure => "installed",
+      require => [ User["mock"] ],
+   }
+}
+
