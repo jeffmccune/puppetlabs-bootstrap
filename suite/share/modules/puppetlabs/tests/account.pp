5e066af708c3d3186253aa2a8b04f4dd91aef5d4
Jeff McCune <jeff@puppetlabs.com>
2010-08-03 16:09:12 -0700

    Added PS accounts, no password.
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- suite/share/modules/puppetlabs/tests/account.pp
+++ suite/share/modules/puppetlabs/tests/account.pp
@@ -1,16 +1,26 @@
 # $1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0 is the hash for "puppet"
 
+puppetlabs::rcfiles { "root": homedir => "/root" }
+
 puppetlabs::account {
   "jeff":
     uid      => 402,
     gid      => 402,
     comment  => "Jeff McCune",
-    password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
-  "root":
-    homedir  => "/root",
-    uid      => 0,
-    gid      => 0,
-    comment  => "root",
-    password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
+    password => '!!';
+  "teyo":
+    uid      => 403,
+    gid      => 403,
+    comment  => "Teyo Tyree",
+    password => '!!';
+  "dan":
+    uid      => 404,
+    gid      => 404,
+    comment  => "Dan Bode",
+    password => '!!';
+  "nan":
+    uid      => 405,
+    gid      => 405,
+    comment  => "Nan Liu",
+    password => '!!';
 }
-
