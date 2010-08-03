a9be9f24d199a148755fe16f3252f6a138d72005
Jeff McCune <jeff@puppetlabs.com>
2010-08-03 15:30:50 -0700

    Added root account management
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- suite/share/modules/puppetlabs/tests/account.pp
+++ suite/share/modules/puppetlabs/tests/account.pp
@@ -1,7 +1,15 @@
+# $1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0 is the hash for "puppet"
+
 puppetlabs::account {
   "jeff":
     uid => 402,
     gid => 402,
     comment => "Jeff McCune",
     password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
+  "root":
+    uid => 0,
+    gid => 0,
+    comment => "root",
+    password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
 }
+
