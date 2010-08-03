a6ac2a847fe911ea2f715da769e123a5b2e2d02d
Jeff McCune <jeff@puppetlabs.com>
2010-08-03 15:57:12 -0700

    Added authorized keys for everyone I have
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- suite/share/modules/puppetlabs/manifests/account.pp
+++ suite/share/modules/puppetlabs/manifests/account.pp
@@ -42,41 +42,36 @@ define puppetlabs::account(
     replace => false,
   }
 ####
-  group { "${name_real}":
-    ensure => present,
-    gid => $gid_real,
+  puppetlabs::rcfiles {
+    "$name_real":
+      user    => "$name_real",
+      group   => "$gid_real",
+      homedir => "$homedir_real";
   }
-  user { "${name_real}":
-    ensure => "present",
-    home => $homedir_real,
-    password => $password_real,
-    uid => $uid_real,
-    gid => $gid_real,
-    groups => $groups_real,
+  group {
+    "${name_real}":
+      ensure => "present",
+      gid    => $gid_real,
+  }
+  user {
+    "${name_real}":
+      ensure   => "present",
+      home     => $homedir_real,
+      password => $password_real,
+      uid      => $uid_real,
+      gid      => $gid_real,
+      groups   => $groups_real,
   }
   file {
-    "$homedir_real":
-      ensure => directory,
+    "${homedir_real}":
+      ensure  => directory,
       require => [ User["$name_real"], Group["$name_real"] ];
-    "${homedir_real}/.bashrc":
-      mode => "0755",
-      source => "${source}/bashrc";
-    "${homedir_real}/.bash_profile":
-      mode => "0755",
-      source => "${source}/bash_profile";
-    "${homedir_real}/.screenrc":
-      source => "${source}/screenrc";
     "${homedir_real}/.ssh":
       ensure => directory,
-      mode => "0700";
+      mode   => "0700";
     "${homedir_real}/.ssh/authorized_keys":
-      source => "${source}/authorized_keys";
-    "${homedir_real}/.vim":
-      ensure => directory,
-      source => "${source}/vim",
-      recurse => true;
-    "${homedir_real}/.vimrc":
-      source => "${source}/vimrc";
+      source => [ "${source}/authorized_keys.${name_real}",
+                  "${source}/authorized_keys.all", ];
   }
 }
 #
--- suite/share/modules/puppetlabs/tests/account.pp
+++ suite/share/modules/puppetlabs/tests/account.pp
@@ -2,14 +2,15 @@
 
 puppetlabs::account {
   "jeff":
-    uid => 402,
-    gid => 402,
-    comment => "Jeff McCune",
+    uid      => 402,
+    gid      => 402,
+    comment  => "Jeff McCune",
     password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
   "root":
-    uid => 0,
-    gid => 0,
-    comment => "root",
+    homedir  => "/root",
+    uid      => 0,
+    gid      => 0,
+    comment  => "root",
     password => '$1$h1sO8/t5$7KwwnndbCBxV3zgwjh.zs0';
 }
 
